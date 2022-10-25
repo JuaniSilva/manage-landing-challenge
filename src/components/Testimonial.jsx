ad2e5246e7d524d792887ab16a13cb164afca81e
JuaniSilva <juanicruzsilva@gmail.com>
2022-10-25 16:16:13 -0300

    fix: image path for deplo?

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -10,7 +10,7 @@ const Testimonial = React.forwardRef(({ name, image, testimonial }, ref) => {
 			className="flex flex-col relative items-center bg-neutralLightGray pt-16 px-8 pb-6 gap-4 w-full shrink-0 max-w-[540px]"
 		>
 			<img
-				src={`../images/${image}`}
+				src={`images/${image}`}
 				alt={`${name} profile picture`}
 				className="max-w-full w-16 h-16 rounded-full absolute -top-8 left-1/2 transform -translate-x-1/2"
 			/>
